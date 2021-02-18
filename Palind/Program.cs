88962005bb92acf52b9014f2a20bbacdba55558c
hallamjason4 <70266362+hallamjason4@users.noreply.github.com>
2021-02-18 16:20:59 +0000

    Add files via upload
    Checks to see if a word is a palindrome or not

---
+++ Palind/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to see if it is a palindrome: ");
+            string checkPal = (Console.ReadLine());
+
+            if (checkPal.SequenceEqual(checkPal.Reverse()) == true){
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
